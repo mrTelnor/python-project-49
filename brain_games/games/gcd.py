f51c770d78f022625bf298bc09ca03d5d19ffaa4
Nikita <mrtelnor@gmail.com>
2025-10-28 13:09:47 +0300

    Update brain game GCD

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,7 +1,16 @@
 import random
-from math import gcd
 
 DESCRIPTION = "Find the greatest common divisor of given numbers."
+
+
+def calc_gcd(a, b):
+    # Вычисляет наибольший общий делитель по алгоритму Евклида
+    while b != 0:
+        temp = b
+        remainder = a % b
+        a = temp
+        b = remainder
+    return a
 
 
 def generate_round():
@@ -9,5 +18,5 @@ def generate_round():
     number1 = random.randint(1, 100)
     number2 = random.randint(1, 100)
     question = f"{number1} {number2}"
-    correct_answer = str(gcd(number1, number2))
+    correct_answer = str(calc_gcd(number1, number2))
     return question, correct_answer
